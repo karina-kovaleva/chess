a7523593b1dee21cd39e7683368d1d955fe9344b
karina-kovaleva <110999528+karina-kovaleva@users.noreply.github.com>
2022-08-17 12:10:30 +0300

    Add files via upload

---
+++ HomeWork12_Karina (chess)/HomeWork12_Karina (chess)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork12_Karina (chess)
+//
+//  Created by Karina Kovaleva on 17.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBOutlet var centralView: UIView!
+    @IBOutlet var blackView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
